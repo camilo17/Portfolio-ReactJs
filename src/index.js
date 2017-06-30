09a8d42f80899cf35a3f932a1c1b3beb8d9aebf9
camilo17 <cami172013@gmail.com>
2017-06-29 21:05:59 -0500

    Created a quick App component and added react router and also made Home component.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-// Set up your application entry point here...
+import React , {Component} from 'react';
+import ReactDOM from 'react-dom'; 
+import {hashHistory, Route, Router } from 'react-router'; 
+
+import Home from './components/Home'
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path="/" component={Home}/>
+            </Router>    
+
+        ); 
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app')); 
